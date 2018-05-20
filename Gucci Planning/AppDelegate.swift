425af1b58b8f4a1d2ccc31d683ffcd3df949dfd2
Daniel Zanchi <danny.zanchi@gmail.com>
2018-05-20 20:01:27 +0200

    Initial Commit

---
+++ Gucci Planning/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Gucci Planning
+//
+//  Created by Daniel Zanchi on 20/05/2018.
+//  Copyright © 2018 Daniel Zanchi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
